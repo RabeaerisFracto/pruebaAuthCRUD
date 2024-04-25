7db80ab05e81c7dc2f90a18e9b6498775ed570e0
Aaron Moncada <flaco426@gmail.com>
2024-04-25 08:39:15 -0400

    pruebaAuthCRUD ==> Hornet-Discrepancias   - Añadida posibilidad de actualizar data si es que ya se encuentra en la base de datos.

--- src/components/discrepancia.tsx
+++ src/components/discrepancia.tsx
@@ -27,24 +27,41 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   //El tipo React.FormEvent<HTMLFormElement> indica que el evento es específico para un elemento de formulario HTML
   //(HTMLFormElement). Esto significa que la función handleSubmit se utilizará como controlador de eventos para un formulario en particular.
   e.preventDefault();
-  const usuario = await client.auth.getUser();
-  console.log(usuario);
+  // const usuario = await client.auth.getUser();
+  // console.log(usuario);
   //esto solo sirve para cuando no es necesario destructurar, sino que se necesita el objeto completo
   //Aqui se puede comprobar en consola que data se divide en user y error
   const { data: { user:otroNombre } } = await client.auth.getUser();
   //se usan {} para desestructurar el objeto, se accede a la propiedad data y se desestructura el objeto user
   //los : se usan para renombrar la variable, en este caso se renombra user a otroNombre
-  console.log(otroNombre);
-  console.log(discrepancia + " " + folio);
+  // console.log(otroNombre);
+  // console.log(discrepancia + " " + folio);
   try{
-    const result = await client.from('Discrepancia').insert([{
+    const {data, error} = await client.from('Discrepancia').insert([{
       discrepancia: discrepancia,
       folio: folio,
       user_id: otroNombre?.id, // ? necesario para que no de error en la consola
       user_name: (otroNombre?.user_metadata.full_name === null ? otroNombre?.email?.split("@")[0] : otroNombre?.user_metadata.full_name),
     }]);
-    console.log(result);
-  }catch(error){
+    console.log(data, error);
+    if(folio !== ""){
+      if(confirm('Discrepancia en folio ya ingresada, desea actualizar??')){
+
+        const update = await client
+        .from('Discrepancia')
+        .update(
+          {
+            discrepancia: discrepancia,
+            user_id: otroNombre?.id,
+            user_name: (otroNombre?.user_metadata.full_name === null ? otroNombre?.email?.split("@")[0] : otroNombre?.user_metadata.full_name),
+          }
+        )
+        .eq('folio', folio);
+        console.log(update);
+        alert('Discrepancia actualizada');
+      }
+
+  }}catch(error){
     console.log("error en CRUD "+error);
   }
 }
