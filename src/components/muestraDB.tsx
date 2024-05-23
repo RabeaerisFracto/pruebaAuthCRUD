7bf03a440de130767fcac529b7d701163bb8e4b0
RabeaerisFracto <flaco426@gmail.com>
2024-05-22 23:58:15 -0400

    Hornet-Discrepancias (r-q.1) -Fetch a consola

--- src/components/muestraDB.tsx
+++ src/components/muestraDB.tsx
@@ -1,8 +1,8 @@
 import { client } from "../supabase/client";
-import { useState, useEffect } from "react";
 import DataTable, { createTheme } from "react-data-table-component";
 import './stylesheets/muestraDB.css'
 // import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/table";
+import { useQuery } from "@tanstack/react-query";
 
 createTheme('prueba', {
     text: {
@@ -24,33 +24,31 @@ interface Discrepancia {
 
 
 export default function MuestraDB() {
-    const [discrepancias, setDiscrepancias] = useState<any>(null);
-    useEffect(() => {
-        async function fetchDiscrepancias() {;
-        const { data, error } = await client.from('Discrepancia')
-        .select('*,RecepciónCarozo(*)');
-        if (error) console.log('error', error);
-        else setDiscrepancias(data);
-        }
-        fetchDiscrepancias();
-    },[]);
+
+
+    const { data: dataDiscrepancias, isLoading, error } = useQuery({
+        queryFn: () => client.from('Discrepancia').select('*,RecepciónCarozo(*)'),
+        queryKey: ['dataDiscrepancias'],
+    });
+    console.log(dataDiscrepancias);
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error de carga de data</div>;
+
+
+
+    // useEffect(() => {
+    //     async function fetchDiscrepancias() {;
+    //     const { data, error } = await client.from('Discrepancia')
+    //     .select('*,RecepciónCarozo(*)');
+    //     if (error) console.log('error', error);
+    //     else setDiscrepancias(data);
+    //     }
+    //     fetchDiscrepancias();
+    // },[]);
 return(
     <>
-    <div className="tabla">
-        <DataTable
-            title="Discrepancias"
-            theme="prueba"
-            columns={[
-                {name: 'Folio', sortable:true,selector:(row: Discrepancia) => row.folio},
-                {name: 'Usuario',grow:2, sortable:true,selector:(row: Discrepancia) => row.user_name},
-                {name: 'Discrepancia',grow:5,sortable:true, selector:(row: Discrepancia) => row.discrepancia},
-                {name: 'Bins', sortable:true,selector:(row: Discrepancia) => row.RecepciónCarozo && row.RecepciónCarozo.Bins},
-                {name: 'SDP', sortable:true,selector:(row: Discrepancia) => row.RecepciónCarozo && row.RecepciónCarozo.SDP},
-                {name: 'Productor',grow:4, sortable:true,selector:(row: Discrepancia) => row.RecepciónCarozo && row.RecepciónCarozo.Nom_prod},
-            ]}
-            data={discrepancias || []}
-        />
-    </div>
+
     </>
 )
     }
--- src/pages/lista.tsx
+++ src/pages/lista.tsx
@@ -1,14 +1,20 @@
 import EsquinaUsuario from '../components/esquinaUsuario';
 import MuestraDB from "../components/muestraDB";
 import '../pages/stylesheets/pags.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+const queryClient = new QueryClient();
 
 function ListaDiscrepancias (){
     return(
+        <QueryClientProvider client={queryClient}>
         <div>
             <EsquinaUsuario/>
             <h1 className='titulo'>Lista Discrepancias</h1>
             <MuestraDB/>
         </div>
+        </QueryClientProvider>
     )
 }
 export default ListaDiscrepancias;
